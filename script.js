244bf9b740950fed1c19d9f081d23a805ce32f9a
antonella1986 <antonella.leo.mail@gmail.com>
2025-02-21 16:44:12 +0100

    add log message

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
 /* Snack 1
 Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-Stampare a schermo la bici con peso minore.
+Stampare a schermo la bici con peso minore.*/
 
-Snack2
-Creare un array di oggetti di squadre di calcio.
-Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
-Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
-Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
 
 const biciclette = [{
-        nome : "Giant TCR Advanced SL",
-        peso : 6.4
-    },{
-        nome : "Scott Addict RC Ultimate",
-        peso : 5.9
-    },{
-        nome : "Factor O2 VAM 2023",
-        peso : 6.32
-    },{
-        nome : "Canyon Ultimate CF SL 8 Aero",
-        peso : 7.86
-    }
+    nome : "Giant TCR Advanced SL",
+    peso : 6.4
+},{
+    nome : "Scott Addict RC Ultimate",
+    peso : 5.9
+},{
+    nome : "Factor O2 VAM 2023",
+    peso : 6.32
+},{
+    nome : "Canyon Ultimate CF SL 8 Aero",
+    peso : 7.86
+}
 ]
 
 let biciclettaLeggera = biciclette[0]
@@ -32,8 +26,14 @@ for (let i = 0; i < biciclette.length; i++) {
     }
 }
 
-console.log(biciclettaLeggera);
+console.log(`La bicicletta più leggera è ${biciclettaLeggera}`);
 
+/*Snack2
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
 
 
 

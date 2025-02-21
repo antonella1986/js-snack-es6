afc7e7109937c01e69a184bbdf204ff49f3f3df1
antonella1986 <antonella.leo.mail@gmail.com>
2025-02-21 16:51:06 +0100

    add array of teams

--- script.js
+++ script.js
@@ -35,5 +35,23 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
 
+const squadre = [{
+    nome : "Milan",
+    puntiFatti : 0,
+    falliSubiti : 0
+},{
+    nome : "Inter",
+    puntiFatti : 0,
+    falliSubiti : 0
+},{
+    nome : "Juve",
+    puntiFatti : 0,
+    falliSubiti : 0
+},{
+    nome : "Bari",
+    puntiFatti : 0,
+    falliSubiti : 0
+}
+]
 
 
